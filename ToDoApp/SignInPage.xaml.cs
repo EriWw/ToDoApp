7b3c8d4617022bee9651e8a27516d143e8311bc4
EriWw <erichongsen@gmail.com>
2026-03-12 00:48:38 +0800

    Added SignUpPage

--- ToDoApp/SignInPage.xaml.cs
+++ ToDoApp/SignInPage.xaml.cs
@@ -14,6 +14,6 @@ public partial class SignInPage : ContentPage
 
     private async void OnSignUpClicked(object sender, EventArgs e)
     {
-
+        await Navigation.PushAsync(new SignUpPage());
     }
 }
