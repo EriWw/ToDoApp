24222f4079b1e7435486d3564bcbf60616e6d3ea
EriWw <erichongsen@gmail.com>
2026-03-12 00:25:26 +0800

    Add project files.

---
+++ ToDoApp/SignInPage.xaml.cs
@@ -0,0 +1,19 @@
+namespace ToDoApp;
+
+public partial class SignInPage : ContentPage
+{
+    public SignInPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnSignInClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+
+    private async void OnSignUpClicked(object sender, EventArgs e)
+    {
+
+    }
+}
