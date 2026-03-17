1989b6087965d13cbb0337acc654b3233b348fd0
actuallynotDeru <crazydean2005@gmail.com>
2026-03-17 08:04:06 +0800

    Add files via upload

---
+++ ToDoApp/TaskStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ToDoApp;
+
+public static class TaskStore
+{
+    // Holds the tasks that are still active (ToDo tab)
+    public static ObservableCollection<ToDoClass> ActiveTasks { get; set; } = new ObservableCollection<ToDoClass>();
+
+    // Holds the tasks that are finished (Completed tab)
+    public static ObservableCollection<ToDoClass> CompletedTasks { get; set; } = new ObservableCollection<ToDoClass>();
+}
